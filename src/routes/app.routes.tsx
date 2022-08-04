c3b8922034fdd6ca17800f89bce1b671a5589107
feliperbdantas <feliperbdantas@outlook.com>
2022-08-04 19:58:57 -0300

    [refactor] - Melhorias do Router

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Dashboard from "../pages/Dashboard";
 
@@ -8,18 +8,33 @@ import Layout from "../components/Layout";
 
 import List from "../pages/List";
 
+import { IRoute } from '../store/types/types';
+
+const routes: Array<IRoute> = [
+    {
+        title: 'Dashboard',
+        auth: true,
+        path: '/dashboard',
+        element: <Dashboard />
+    },
+    {
+        title: 'Listagem',
+        auth: true,
+        path: '/list/:type',
+        element: <List />
+    }
+];
+
 const AppRoutes: React.FC = () => (
     <Layout>
         <Routes>
-            <Route 
-                path="/dashboard" 
-                element={ <Dashboard /> } 
-            />
-
-            <Route 
-                path="/list/:type" 
-                element={ <List /> } 
-            />
+            { routes.map((route, index) => (
+                <Route 
+                    key={ index }
+                    path={ route.path } 
+                    element={ route.element } 
+                />
+            )) }
         </Routes>
     </Layout>
 );
--- src/store/types/types.ts
+++ src/store/types/types.ts
@@ -4,3 +4,10 @@ export interface ISelectInputProps {
         label: string | number;
     }[],
 }
+
+export interface IRoute {
+    title: string;
+    auth: boolean;
+    path: string;
+    element: JSX.Element;
+}

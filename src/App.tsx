ed3f71c188585972ae300b29e3fedab5943f1ed8
feliperbdantas <feliperbdantas@outlook.com>
2022-08-03 21:31:38 -0300

    [feat] - Implementação do History Finance Card Item da página de Listagem de Entradas e Saídas

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,14 @@ import dark from "./styles/themes/dark";
 
 import Layout from "./components/Layout";
 
-import Dashboard from "./pages/Dashboard";
+import List from "./pages/List";
 
 const App: React.FC = () => {
     return (
         <ThemeProvider theme={dark}>
             <GlobalStyles />
             <Layout>
-                <Dashboard />
+                <List />
             </Layout>
         </ThemeProvider>
     );
--- src/store/enums/enum.ts
+++ src/store/enums/enum.ts
@@ -3,3 +3,13 @@ export const ContentHeaderOptions = [
     { value: 'Alef Felipe', label: 'Alef Felipe' },
     { value: 'Vinícius Cavasani', label: 'Vinícius Cavasani' }
 ];
+
+export const ListItemHistoryFinanceCard = [
+    {
+        cardColor: '#313862',
+        tagColor: '#E44C4E',
+        title: 'Conta de Luz',
+        subtitle: '03/08/2022',
+        amount: '130,00'
+    },
+];

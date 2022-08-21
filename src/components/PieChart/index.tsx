4fb0bf3bd2af9d959be5526cb8fa9ec8bc448fe2
feliperbdantas <feliperbdantas@outlook.com>
2022-08-21 19:05:06 -0300

    [feature] - Finalização da Implementação das Legendas do PieChart

--- src/components/PieChart/index.tsx
+++ src/components/PieChart/index.tsx
@@ -16,8 +16,13 @@ import { Container, SideLeft, LegendContainer, Legend, SideRight } from "./style
 const PieChart: React.FC = () => (
   <Container>
     <SideLeft>
+      <h2>Relação</h2>
       <LegendContainer>
-        <Legend>
+        <Legend color='#F7931B'>
+          <div>5%</div>
+          <span>Entradas</span>
+        </Legend>
+        <Legend color='#E44C4E'>
           <div>95%</div>
           <span>Saídas</span>
         </Legend>
--- src/components/PieChart/styles.ts
+++ src/components/PieChart/styles.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+interface ILegendProps {
+  color: string;
+}
+
 export const Container = styled.div`
   display: flex;
   width: 48%;
@@ -10,18 +14,54 @@ export const Container = styled.div`
   color: ${ props=> props.theme.colors.white };
 `;
 
-export const SideLeft = styled.div`
+export const SideLeft = styled.aside`
+  padding: 30px 20px;
 
+  > h2 {
+    margin-bottom: 20px;
+  }
 `;
 
-export const LegendContainer = styled.div`
+export const LegendContainer = styled.ul`
+  height: 175px;
+  padding-right: 15px;
+  list-style: none;
+  overflow-y: scroll;
 
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: ${ props => props.theme.colors.secondary };
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: ${ props => props.theme.colors.tertiary };
+  }
 `;
 
-export const Legend = styled.div`
+export const Legend = styled.li<ILegendProps>`
+  display: flex;
+  align-items: center;
+  margin-bottom: 7px;
+  
+  > div {
+    background-color: ${ props => props.color };
+    width: 40px;
+    height: 40px;
+    border-radius: 5px;
+    font-size: 18px;
+    text-align: center;
+    line-height: 40px;
+  }
 
+  > span {
+    margin-left: 5px;
+  }
 `;
 
-export const SideRight = styled.div`
+export const SideRight = styled.main`
 
 `;

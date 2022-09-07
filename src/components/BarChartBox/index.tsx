a6d56978535c4f241ef2b5e14118bd392a397011
feliperbdantas <feliperbdantas@outlook.com>
2022-09-07 14:14:57 -0300

    [feat] - Implementação do gráfico de relação de entradas e saídas de movimento recorrente versus eventual

--- src/components/BarChartBox/index.tsx
+++ src/components/BarChartBox/index.tsx
@@ -1,13 +1,80 @@
 import React from "react";
 
+// COMPONENTS
+
+import { v4 as uuidv4 } from 'uuid';
+
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  Cell
+} from 'recharts';
+
 // STYLES
 
-import { Container } from './styles';
+import { 
+  Container, 
+  Legend, 
+  LegendContainer, 
+  SideLeft, 
+  SideRight 
+} from '@components/BarChartBox/styles';
 
-const BarChartBox: React.FC = () => {
+interface IBarChartBoxProps {
+  title: string;
+  data: {
+    name: string;
+    amount: number;
+    percent: number;
+    color: string;
+  }[];
+}
+
+const BarChartBox: React.FC<IBarChartBoxProps> = ({ title, data }) => {
   return (
     <Container>
-      
+      <SideLeft>
+        <h2>{ title }</h2>
+
+        <LegendContainer>
+        {
+          data && data.map((indicator) => (
+            <Legend color={ indicator.color } key={ uuidv4() }>
+              <div>{ indicator.percent || '--' }</div>
+              <span>{ indicator.name }</span>
+            </Legend>
+          ))
+        }
+      </LegendContainer>
+      </SideLeft>
+
+      <SideRight>
+        <ResponsiveContainer>
+          <BarChart 
+            data={ data } 
+            width={500}
+            height={300}
+            margin={{
+              top: 5,
+              right: 10,
+              left: 10,
+              bottom: 5,
+            }}
+            barSize={80}
+          >
+            <Bar dataKey="amount">
+              { data.map((indicator) => (
+                <Cell 
+                  key={ indicator.name }
+                  cursor="pointer"
+                  fill={ indicator.color }
+                />
+              )) }
+            </Bar>
+          </BarChart>
+        </ResponsiveContainer>
+      </SideRight>
     </Container>
   );
 }
--- src/components/BarChartBox/styles.ts
+++ src/components/BarChartBox/styles.ts
@@ -1,5 +1,77 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+interface ILegendProps {
+  color: string;
+}
 
+export const Container = styled.div`
+  display: flex;
+  width: 48%;
+  min-height: 260px;
+  margin: 10px 0;
+  background-color: ${ props => props.theme.colors.tertiary };
+  color: ${ props => props.theme.colors.white };
+  border-radius: 7px;
+`;
+
+export const SideLeft = styled.aside`
+  padding: 30px 20px;
+
+  > h2 {
+    padding-left: 16px;
+    margin-bottom: 10px;
+  }
+`;
+
+export const SideRight = styled.main`
+  display: flex;
+  justify-content: center;
+  flex: 1;
+  min-height: 150px;
+  padding-top: 35px;
+`;
+
+export const LegendContainer = styled.ul`
+  height: 175px;
+  padding-right: 15px;
+  list-style: none;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: ${ props => props.theme.colors.secondary };
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: ${ props => props.theme.colors.tertiary };
+  }
+`;
+
+export const Legend = styled.li<ILegendProps>`
+  display: flex;
+  align-items: center;
+  margin-bottom: 7px;
+  padding-left: 16px;
+  
+  > div {
+    background-color: ${ props => props.color };
+    width: 60px;
+    height: 40px;
+    border-radius: 5px;
+    font-size: 18px;
+    text-align: center;
+    line-height: 40px;
+
+    &::after {
+      content: '%';
+    }
+  }
+
+  > span {
+    margin-left: 5px;
+  }
 `;

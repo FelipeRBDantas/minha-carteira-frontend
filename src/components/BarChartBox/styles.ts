d089dbcb6ec1287c47ca1c8db72cf4493245925c
feliperbdantas <feliperbdantas@outlook.com>
2022-10-18 20:12:43 -0300

    [feat] - Responsividade: Gráfico de Barras

--- src/components/BarChartBox/styles.ts
+++ src/components/BarChartBox/styles.ts
@@ -12,6 +12,13 @@ export const Container = styled.div`
   background-color: ${ props => props.theme.colors.tertiary };
   color: ${ props => props.theme.colors.white };
   border-radius: 7px;
+
+  @media (max-width: 1200px) {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: auto;
+  }
 `;
 
 export const SideLeft = styled.aside`
@@ -49,6 +56,11 @@ export const LegendContainer = styled.ul`
   &::-webkit-scrollbar-track {
     background-color: ${ props => props.theme.colors.tertiary };
   }
+
+  @media (max-width: 1200px) {
+    display: flex;
+    height: auto;
+  }
 `;
 
 export const Legend = styled.li<ILegendProps>`
@@ -59,10 +71,10 @@ export const Legend = styled.li<ILegendProps>`
   
   > div {
     background-color: ${ props => props.color };
-    width: 60px;
+    width: 56px;
     height: 40px;
     border-radius: 5px;
-    font-size: 18px;
+    font-size: 16px;
     text-align: center;
     line-height: 40px;
 
@@ -73,5 +85,17 @@ export const Legend = styled.li<ILegendProps>`
 
   > span {
     margin-left: 5px;
+  }
+
+  @media (max-width: 1200px) {
+    > div {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 40px;
+      height: 36px;
+      font-size: 12px;
+      line-height: 30px;
+    }
   }
 `;
